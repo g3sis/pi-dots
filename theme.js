07d91662a3dba425c91cf8ea9cc404fcd73a8395
Robin <ge54sis@mytum.de>
2022-11-22 16:19:42 +0100

    last theme broke my shell

--- theme.js
+++ theme.js
@@ -1,6 +1,4 @@
-t.prefs_.set('color-palette-overrides',[]"#161616", "#262626", "#393939", "#525252", "#dde1e6", "#f2f4f8", "#ffffff", "#08bdba",
-            "#3ddbd9", "#78a9ff", "#ee5396", "#33b1ff", "#ff7eb6", "#42be65", "#be95ff", "#82cfff",
-            "#131313", "",);
+t.prefs_.set('color-palette-overrides',["#161616", "#262626", "#393939", "#525252", "#dde1e6", "#f2f4f8", "#ffffff", "#08bdba","#3ddbd9", "#78a9ff", "#ee5396", "#33b1ff", "#ff7eb6", "#42be65", "#be95ff", "#82cfff","#131313"]);
 t.prefs_.set('foreground-color', "#999993");
 t.prefs_.set('background-color', "#000000");
 t.prefs_.set('cursor-color', 'rgba(158,158,203,0.5)');
