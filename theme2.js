0a9d693a9d7eea90f09d8dfc1e8e8ccb326aaf5c
Robin <ge54sis@mytum.de>
2022-11-24 09:03:04 +0100

    last theme broke my shell

---
+++ theme2.js
@@ -0,0 +1,26 @@
+black       = '#000000';
+red         = '#ee5396'; // red
+green       = '#42be65'; // green
+yellow      = '#9F9F8F'; // yellow
+blue        = '#33b1ff'; // blue
+lavender    = '#be95ff'; // lavender
+cyan        = '#82cfff'; // cyan
+white       = '#ffffff'; // light gray
+lightBlack  = '#C2E8FF'; // medium gray
+lightRed    = '#ff7eb6'; // red
+lightGreen  = '#529B2F'; // green
+lightYellow = '#9F9F8F'; // yellow
+lightBlue   = '#08bdba'; // blue
+lightMagenta= '#F92672'; // pink
+lightCyan   = '#28C6E4'; // cyan
+lightWhite  = '#E0E0E0'; // white
+ 
+t.prefs_.set('color-palette-overrides',
+                 [ black , red     , green  , yellow,
+                  blue     , magenta , cyan   , white,
+                  lightBlack   , lightRed  , lightGreen , lightYellow,
+                  lightBlue    , lightMagenta  , lightCyan  , lightWhite ]);
+ 
+t.prefs_.set('cursor-color', 'rgba(0, 0, 0, 0.5)');
+t.prefs_.set('foreground-color', '#000000');
+t.prefs_.set('background-color', white);
