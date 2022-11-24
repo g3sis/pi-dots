0f912bed0567d4b2490976e41c78dd41c843c726
Robin <ge54sis@mytum.de>
2022-11-24 09:10:58 +0100

    last theme broke my shell

--- theme2.js
+++ theme2.js
@@ -22,5 +22,5 @@ t.prefs_.set('color-palette-overrides',
                   lightBlue    , lightMagenta  , lightCyan  , lightWhite ]);
  
 t.prefs_.set('cursor-color', 'rgba(0, 0, 0, 0.5)');
-t.prefs_.set('foreground-color', '#000000');
-t.prefs_.set('background-color', white);
+t.prefs_.set('foreground-color', white);
+t.prefs_.set('background-color', black);
