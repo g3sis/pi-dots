08b9808507cd90408fa17c1cda6e81e5a068241f
Robin <ge54sis@mytum.de>
2022-12-11 17:48:15 +0100

    last theme broke my shell

--- blink/oxocarbon.js
+++ blink/oxocarbon.js
@@ -20,27 +20,27 @@ base0F = '#82cfff';
 
 t.prefs_.set('color-palette-overrides', 
                         [base00,
-                        base06,
+                        base08,
                         base0B,
                         base0A,
                         base0D,
                         base0E,
-                        base0C,
-                        base05,
                         base03,
-                        base06,
+                        base05,
+                        base0C,
+                        base08,
                         base0B,
                         base0A,
                         base0D,
                         base0E,
-                        base0C,
+                        base03,
                         base07,
                         base09,
                         base0F,
                         base01,
                         base02,
                         base04,
-                        base08]);
+                        base06]);
 
 t.prefs_.set('cursor-color', "rgba(242, 244, 248,0.5)");
 t.prefs_.set('foreground-color', base05);
