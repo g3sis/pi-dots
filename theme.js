d87f8a28c1c75a40986efc5995fa6d2bd0675dce
Robin <ge54sis@mytum.de>
2022-12-11 17:10:50 +0100

    last theme broke my shell

--- theme.js
+++ theme.js
@@ -1,4 +1,47 @@
-t.prefs_.set('color-palette-overrides',["#161616", "#08bdba","#3ddbd9", "#78a9ff", "#ee5396", "#33b1ff", "#ff7eb6", "#42be65", "#be95ff", "#82cfff","#131313"]);
-t.prefs_.set('foreground-color', "#999993");
-t.prefs_.set('background-color', "#000000");
-t.prefs_.set('cursor-color', 'rgba(158,158,203,0.5)');
+// Base16 {{scheme-name}}
+// Scheme: {{scheme-author}}
+
+base00 = '#161616';
+base01 = '#262626';
+base02 = '#393939';
+base03 = '#525252';
+base04 = '#dde1e6';
+base05 = '#f2f4f8';
+base06 = '#ffffff';
+base07 = '#08bdba';
+base08 = '#3ddbd9';
+base09 = '#78a9ff';
+base0A = '#ee5396';
+base0B = '#33b1ff';
+base0C = '#ff7eb6';
+base0D = '#42be65';
+base0E = '#be95ff';
+base0F = '#82cfff';
+
+t.prefs_.set('color-palette-overrides', 
+                        [base00,
+                        base08,
+                        base0B,
+                        base0A,
+                        base0D,
+                        base0E,
+                        base0C,
+                        base05,
+                        base03,
+                        base08,
+                        base0B,
+                        base0A,
+                        base0D,
+                        base0E,
+                        base0C,
+                        base07,
+                        base09,
+                        base0F,
+                        base01,
+                        base02,
+                        base04,
+                        base06]);
+
+t.prefs_.set('cursor-color', "rgba({{base05-rgb-r}}, {{base05-rgb-g}}, {{base05-rgb-b}}, 0.5)");
+t.prefs_.set('foreground-color', base05);
+t.prefs_.set('background-color', base00);
